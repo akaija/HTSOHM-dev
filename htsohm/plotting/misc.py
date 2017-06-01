4c13f247d2c86213a9908c14edb940a79690dfc4
A Reino Kaija <a.r.kaija@gmail.com>
2017-06-01 11:32:16 -0400

    added method for plotting bin-count-variance convergence

--- htsohm/plotting/misc.py
+++ htsohm/plotting/misc.py
@@ -1,6 +1,7 @@
 import os
 
 import matplotlib.pyplot as plt
+import numpy as np
 from sqlalchemy.sql import func
 from sqlalchemy import or_
 
@@ -55,5 +56,49 @@ def plot_empty_bin_convergence(run_id, max_generation = None):
     plt.ylabel('Number of empty bins')
     plt.plot(range(len(data)), data)
     plt.savefig('{}_MaxGen{}_EmptyBins.png'.format(run_id, max_generation))
+
+def query_normalised_bin_count_variances(run_id, max_generation):
+    htsohm_dir = os.path.dirname(os.path.dirname(htsohm.__file__))
+    config_path = os.path.join(htsohm_dir, run_id, 'config.yaml')
+    config = load_config_file(config_path)
+
+    filters = [Material.run_id == run_id, or_(Material.retest_passed == True,
+        Material.retest_passed == None),
+        Material.generation_index < config['children_per_generation']]
+
+    variances = []
+    for generation in range(max_generation):
+        bin_count_tuples = session \
+                .query(func.count(Material.uuid)) \
+                .filter(*filters, Material.generation <= generation) \
+                .group_by(Material.gas_adsorption_bin,
+                        Material.surface_area_bin, Material.void_fraction_bin) \
+                .all()
+        bin_counts = [e[0] for e in bin_count_tuples]
+        normalised_bin_counts = [e / max(bin_counts) for e in bin_counts]
+        variances.append(np.var(normalised_bin_counts))
+
+    return variances
+
+def plot_variance_convergence(run_id, max_generation = None):
+    htsohm_dir = os.path.dirname(os.path.dirname(htsohm.__file__))
+    config_path = os.path.join(htsohm_dir, run_id, 'config.yaml')
+    config = load_config_file(config_path)
+
+    filters = [Material.run_id == run_id, or_(Material.retest_passed == True,
+        Material.retest_passed == None),
+        Material.generation_index < config['children_per_generation']]
+
+    if max_generation == None:
+        max_generation = session.query(func.max(Material.generation)) \
+                .filter(*filters).one()[0]
+
+    variances = query_normalised_bin_count_variances(run_id, max_generation)
+
+    fig = plt.figure(figsize = (12, 8))
+    plt.xlabel('Generation')
+    plt.ylabel('Normalised bin-count variance')
+    plt.plot(range(len(variances)), variances)
+    plt.savefig('{}_MaxGen{}_Variance.png'.format(run_id, max_generation))
 
 
