9ed80624b8acf2aefae33a0ee5ebd1288a578437
A Reino Kaija <a.r.kaija@gmail.com>
2017-10-04 17:25:41 -0400

    created template for force_field_mixing_rules.def

--- htsohm/simulation/forcefield/files.py
+++ htsohm/simulation/forcefield/files.py
@@ -52,48 +52,18 @@ def write_mixing_rules(material, simulation_path):
     """Writes .def file for forcefield information.
 
     """
-    adsorbate_LJ_atoms = [
-            ['N_n2',    36.0,       3.31],
-            ['C_co2',   27.0,       2.80],
-            ['O_co2',   79.0,       3.05],
-            ['CH4_sp3', 158.5,      3.72],
-            ['He',      10.9,       2.64],
-            ['H_com',   36.7,       2.958],
-            ['Kr',      167.06,     3.924],
-            ['Xe',      110.704,    3.690]
-    ]
- 
-    adsorbate_none_atoms = ['N_com', 'H_h2']
+    values = {'NumberOfDefinedInteractions' : len(material.structure.lennard_jones) + 10}
+    mix_template = load_and_subs_template('force_field_mixing_rules.def', values)
 
     file_name = os.path.join(simulation_path, 'force_field_mixing_rules.def')
     with open(file_name, "w") as mixing_rules_file:
-        mixing_rules_file.write(
-            "# general rule for shifted vs truncated\n" +
-            "shifted\n" +
-            "# general rule tailcorrections\n" +
-            "no\n" +
-            "# number of defined interactions\n" +
-            "{}\n".format(len(material.structure.lennard_jones) + 10) +
-            "# type interaction, parameters.    " +
-            "IMPORTANT: define shortest matches first, so" +
-            " that more specific ones overwrites these\n"
-        )
+        mixing_rules_file.write(mix_template)
         for lennard_jones in material.structure.lennard_jones:
             mixing_rules_file.write(
                 "{0:12} lennard-jones {1:8f} {2:8f}\n".format(
                     lennard_jones.chemical_id,
                     round(lennard_jones.epsilon, 4),
-                    round(lennard_jones.sigma, 4)
-                )
-            )
-        for at in adsorbate_LJ_atoms:
-            mixing_rules_file.write(
-                "{0:12} lennard-jones {1:8f} {2:8f}\n".format(at[0], at[1], at[2])
-            )
-        for at in adsorbate_none_atoms:
-            mixing_rules_file.write(
-                "{0:12} none\n".format(at)
-            )
+                    round(lennard_jones.sigma, 4)))
         mixing_rules_file.write(
             "# general mixing rule for Lennard-Jones\n" +
             "Lorentz-Berthelot")
